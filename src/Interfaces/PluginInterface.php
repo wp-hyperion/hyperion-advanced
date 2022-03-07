d9e4ada9ef58bdce2399fe43a0772e4159ee6dab
d4rk0s <gregory@d4rk0s.net>
2022-03-07 18:02:36 +0100

    remove

--- src/Interfaces/PluginInterface.php
+++ src/Interfaces/PluginInterface.php
@@ -4,7 +4,4 @@ namespace Hyperion\Interfaces;
 
 interface PluginInterface
 {
-    public static function poweringUp();
-    public static function shuttingDown();
-    public static function ignition();
 }
